2df644fb88e78253a7cbdbad67bbb191269bce28
KyraJ11 <kyleejohn30@gmail.com>
2025-11-25 22:11:16 -0500

    Added start button to start background

---
+++ StartButton.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class StartButton here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class StartButton extends Actor
+{
+    public StartButton(){
+        GreenfootImage image = getImage();
+        image.scale(300,250);
+        setImage(image);
+    }
+    /**
+     * Act - do whatever the StartButton wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
