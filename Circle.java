257379d202d38c37798a3dbfee7556341dae0ef7
KyraJ11 <kyleejohn30@gmail.com>
2025-11-28 15:59:17 -0500

    Added Shapes
    Added the shape images (circle, square, triangle, star, rectangle) and started creating a Shape class.

---
+++ Circle.java
@@ -0,0 +1,37 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Circle here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Circle extends Shapes
+{
+    private int x;
+    private int y;
+    private World world = getWorld();
+    public Circle(){
+        x = world.getWidth()/2;
+        y = world.getHeight()/2;
+        setImage("White-Circle.png");
+    }
+    public Circle(int x, int y){
+        this.x = x;
+        this.y = y;
+        setImage("White-Circle.png");
+    }
+    public Circle(int x, int y, String img){
+        this.x = x;
+        this.y = y;
+        setImage("img");
+    }
+    /**
+     * Act - do whatever the Circle wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
---
+++ Shapes.java
@@ -0,0 +1,34 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Shapes here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Shapes extends Actor
+{
+    int x;
+    int y;
+    private World world = getWorld();
+    
+    public Shapes(String img){
+        x = world.getWidth()/2;
+        y = world.getHeight()/2;
+        setImage(img);
+    }
+    public Shapes(int x, int y, String img){
+        this.x = x;
+        this.y = y;
+        setImage(img);
+    }
+    
+    /**
+     * Act - do whatever the Shapes wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
